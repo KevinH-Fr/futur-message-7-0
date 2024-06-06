92c375f4231bec9c1fc4bc2eff0fcb4f0acc844b
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-06-06 22:09:05 +0200

    modifs pour stimulus sur ios test

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+pin "jquery", to: "jquery.min.js", preload: false
+pin "jquery_ujs", to: "jquery_ujs.js", preload: false
+
+
 pin_all_from "app/javascript/controllers", under: "controllers"
